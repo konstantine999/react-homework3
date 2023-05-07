6ee043a0b0bb1b109f7020c93c89915ed60c0848
konstantine999 <kotemuzashvili0@gmail.com>
2023-05-07 19:58:32 +0400

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,11 @@ function Header() {
   return (
     <header>
       <div className="header-wrapper">
-        <img className="header-background" src={HeaderBackground} />
+        <img
+          className="header-background"
+          src={HeaderBackground}
+          alt="header-background"
+        />
       </div>
     </header>
   );
